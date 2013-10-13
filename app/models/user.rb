607775e9c102222334958ea22be952a0f1a41756
Josh Silverman <joshs.silverman@gmail.com>
2013-10-13 14:31:46 -0400

    refactor todays_goal on user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,13 +1,17 @@
 class User < ActiveRecord::Base
 
+  def todays_goal
+    @_todays_goal ||= Task.where('complete = ? OR complete IS NULL', false).order(:rank).limit(1).first
+  end
+
   def send_todays_goal user
-    top_ranked_task = Task.where('complete = ? OR complete IS NULL', false).order(:rank).limit(1).first
+    top_ranked_task = todays_goal
     msg = "Today's goal: #{top_ranked_task.name}"
     sms(user, msg)
   end
 
   def send_reminder user
-    top_ranked_task = Task.order(:rank).limit(1).first.name
+    top_ranked_task = todays_goal
     msg = "Don't forget your goal for today: #{top_ranked_task}"
     sms(user, msg)
   end
